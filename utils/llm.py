d1d0d60ff42670a28931672c96053ece979bc0eb
AS-MAC-1279 <as-mac-1279@Priyanshu-tomar-MacBook-Air.local>
2026-05-13 01:19:45 +0530

    Integrated Supabase cloud storage

--- utils/llm.py
+++ utils/llm.py
@@ -1,10 +1,14 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
+
 
 load_dotenv()
 
+
 llm = ChatAnthropic(
     model="claude-3-5-sonnet-20241022",
+    anthropic_api_key=os.getenv("ANTHROPIC_API_KEY"),
     temperature=0
 )
